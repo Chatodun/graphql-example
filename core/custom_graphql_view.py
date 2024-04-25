109eb539a276c7c71ebf60f12631ef22e885965e
sergei <sergey.savelev@zensoft.io>
2024-04-25 22:37:32 +0600

    - added small fix for graphql view

--- core/custom_graphql_view.py
+++ core/custom_graphql_view.py
@@ -8,9 +8,10 @@ class CustomGraphQLView(GraphQLView):
         result = super().execute_graphql_request(
             request, data, query, variables, operation_name, show_graphiql
         )
-        if "user" in result.data and request.user.has_perm("auth.view_user") is False:
-            result.data["user"] = {'message': 'Cannot query field "user" on type "Query"'}
-        elif 'type' in result.data and request.user.has_perm("auth.view_user") is False:
-            if 'fields' in result.data['type']:
-                result.data['type']['fields'] = [d for d in result.data['type']['fields'] if d['name'] != 'user']
+        if result.data:
+            if "user" in result.data and request.user.has_perm("auth.view_user") is False:
+                result.data["user"] = {'message': 'Cannot query field "user" on type "Query"'}
+            elif 'type' in result.data and request.user.has_perm("auth.view_user") is False:
+                if 'fields' in result.data['type']:
+                    result.data['type']['fields'] = [d for d in result.data['type']['fields'] if d['name'] != 'user']
         return result
